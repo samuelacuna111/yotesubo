2e791b1f21546dcd73c96ecfad32e608d66590a9
Samuel <samuel.acuna1901@gmail.com>
2024-09-12 19:31:58 -0300

    Start app

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage {
+  email: string = '';
+  password: string= '';
+
+  constructor(private navCtrl: NavController) {}
+
+  onLogin() {
+    if (this.email === 'admin@example.com' && this.password === 'admin123') {
+      // Si el login es exitoso, redirigir a la página principal
+      this.navCtrl.navigateRoot('/home');
+    } else {
+      alert('Credenciales incorrectas');
+    }
+  }
+}
