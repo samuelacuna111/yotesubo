d3d6619685d7ffba3ab8f233ffe04de9c83f69ed
Josevass96 <jo.vasquezp@duocuc.cl>
2024-09-21 01:01:04 -0300

    Avance reestablecer pagina

--- src/app/restablecerpass/restablecerpass.page.ts
+++ src/app/restablecerpass/restablecerpass.page.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-restablecerpass',
   templateUrl: './restablecerpass.page.html',
   styleUrls: ['./restablecerpass.page.scss'],
 })
-export class RestablecerpassPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+    export class RestablecerpassPage {
+    email: string = '';
+    password: string= '';
+  
+    constructor(private navCtrl: NavController) {}
+  
+    onLogin() {
+      if (this.email === 'admin' && this.password === 'admin') {
+        // Si el login es exitoso, redirigir a la página principal
+        this.navCtrl.navigateRoot('/home');
+      } else {
+        alert('Credenciales incorrectas');
+      }
+    }
   }
 
-}
+  
+
