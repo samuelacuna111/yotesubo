d3bf6e7044644d59c5b7e6d04b4bfddc579d0432
Josevass96 <jo.vasquezp@duocuc.cl>
2024-09-23 21:25:55 -0300

    Avance 03

--- src/app/restablecerpass/restablecerpass.page.ts
+++ src/app/restablecerpass/restablecerpass.page.ts
@@ -1,26 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-import { NavController } from '@ionic/angular';
+import { Component } from '@angular/core';
+import { NavController, AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-restablecerpass',
   templateUrl: './restablecerpass.page.html',
   styleUrls: ['./restablecerpass.page.scss'],
 })
-    export class RestablecerpassPage {
-    email: string = '';
-    password: string= '';
-  
-    constructor(private navCtrl: NavController) {}
-  
-    onLogin() {
-      if (this.email === 'admin' && this.password === 'admin') {
-        // Si el login es exitoso, redirigir a la página principal
-        this.navCtrl.navigateRoot('/home');
-      } else {
-        alert('Credenciales incorrectas');
-      }
+export class RestablecerpassPage {
+  email: string = '';
+
+  constructor(private navCtrl: NavController, private alertController: AlertController) {}
+
+  async onSendCode() {
+    if (this.email) {
+      // Crea la alerta
+      const alert = await this.alertController.create({
+        header: 'Código Enviado',
+        message: `Se ha enviado un código a ${this.email}`,
+        buttons: [{
+          text: 'OK',
+          handler: () => {
+            // Redirige a la página de ingreso de código
+            this.navCtrl.navigateForward('/ingreso-codigo');
+          }
+        }]
+      });
+      await alert.present();
+    } else {
+      // Muestra alerta si el correo está vacío
+      const alert = await this.alertController.create({
+        header: 'Error',
+        message: 'Por favor, ingrese un correo válido.',
+        buttons: ['OK']
+      });
+      await alert.present();
     }
   }
-
-  
-
+}
