17634f4bc4fe2c48829a5fc12075a19918dcfa2c
Samuel <samuel.acuna1901@gmail.com>
2024-09-23 21:37:45 -0300

    general

---
+++ src/app/pages/crearviaje/crearviaje.page.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crearviaje',
+  templateUrl: './crearviaje.page.html',
+  styleUrls: ['./crearviaje.page.scss'],
+})
+export class CrearviajePage implements OnInit {
+
+  today: string;
+
+  constructor() {
+    const now = new Date();
+    // Formato: YYYY-MM-DDTHH:mm
+    this.today = now.toISOString();
+  }
+  ngOnInit() {
+  }
+  
+  customCounterFormatter(inputLength: number, maxLength: number) {
+    return `${maxLength - inputLength} characters remaining`;
+  }
+
+  incrementSeats() {
+    const input = document.getElementById('asientosDisponibles') as HTMLInputElement;
+    let currentValue = parseInt(input.value, 10);
+    if (currentValue < 4) { // Cambia 10 por el máximo que desees
+      input.value = (currentValue + 1).toString();
+    }
+  }
+  
+  decrementSeats() {
+    const input = document.getElementById('asientosDisponibles') as HTMLInputElement;
+    let currentValue = parseInt(input.value, 10);
+    if (currentValue > 1) { // Cambia 1 por el mínimo que desees
+      input.value = (currentValue - 1).toString();
+    }
+  }
+  
+
+}
